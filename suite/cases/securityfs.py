59a5bc1e92e12d2a56d6ef4c12d6e2d4877442b2
Fan Wu <wufan@kernel.org>
2026-08-22 11:54:35 -0700

    ipe: test unprivileged policy version reads
    Read version before and after dropping CAP_MAC_ADMIN and compare the values.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/securityfs.py
+++ suite/cases/securityfs.py
@@ -547,4 +547,15 @@ def build():
             expect=0,
             check=checks.two_values_match,
         ),
+        Case(
+            id="read_version_nocap_ok",
+            setup=(
+                partial(steps.deploy_policy, CAPABILITY_POLICY_V1),
+                partial(steps.read_node, "version", CAPABILITY_POLICY_NAME, read_values),
+                steps.drop_mac_admin,
+            ),
+            trigger=partial(triggers.read_node, "version", CAPABILITY_POLICY_NAME, read_values),
+            expect=0,
+            check=checks.two_values_match,
+        ),
     )
