dfd8495d08c8174aacfeb612bc59312d8307369f
Fan Wu <wufan@kernel.org>
2026-08-22 11:54:20 -0700

    ipe: test unprivileged policy name reads
    Read name before and after dropping CAP_MAC_ADMIN and compare the values.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/securityfs.py
+++ suite/cases/securityfs.py
@@ -525,4 +525,15 @@ def build():
             expect=0,
             check=checks.two_values_match,
         ),
+        Case(
+            id="read_name_nocap_ok",
+            setup=(
+                partial(steps.deploy_policy, CAPABILITY_POLICY_V1),
+                partial(steps.read_node, "name", CAPABILITY_POLICY_NAME, read_values),
+                steps.drop_mac_admin,
+            ),
+            trigger=partial(triggers.read_node, "name", CAPABILITY_POLICY_NAME, read_values),
+            expect=0,
+            check=checks.two_values_match,
+        ),
     )
