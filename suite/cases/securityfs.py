d8d33a9d43701f6d5545ad814c5f33cbb728bf94
Fan Wu <wufan@kernel.org>
2026-08-21 23:39:23 -0700

    ipe: test privileged policy updates
    Add a privileged update case and check that the policy version advances
    to 0.0.2.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/securityfs.py
+++ suite/cases/securityfs.py
@@ -12,6 +12,7 @@ from assets import (
     CAPABILITY_POLICY_V1,
     CAPABILITY_POLICY_V1_VERSION,
     CAPABILITY_POLICY_V2,
+    CAPABILITY_POLICY_V2_VERSION,
 )
 from model import Case
 
@@ -32,5 +33,17 @@ def build():
             ),
             expect=errno.EPERM,
             check=partial(checks.policy_version_is, CAPABILITY_POLICY_NAME, CAPABILITY_POLICY_V1_VERSION),
+        ),
+        Case(
+            id="cap_update_withcap_ok",
+            setup=(partial(steps.deploy_policy, CAPABILITY_POLICY_V1),),
+            trigger=partial(
+                triggers.write_node,
+                "update",
+                CAPABILITY_POLICY_NAME,
+                ipe.signed_policy(CAPABILITY_POLICY_V2),
+            ),
+            expect=0,
+            check=partial(checks.policy_version_is, CAPABILITY_POLICY_NAME, CAPABILITY_POLICY_V2_VERSION),
         ),
     )
