2677c7d5b9210e7a1fe8ea3841becb83059e5400
Fan Wu <wufan@kernel.org>
2026-08-22 22:36:39 -0700

    ipe: test equal policy updates
    Require ESTALE when updating a policy with its own version.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/policy.py
+++ suite/cases/policy.py
@@ -1,9 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
+import errno
 from functools import partial
 
 import checks
 import ipe
+import steps
 import triggers
 from assets import POLICY_FIXTURE_NAME, POLICY_FIXTURE_V1_ASSET, POLICY_FIXTURE_V1_VERSION
 from model import Case
@@ -21,6 +23,18 @@ def build():
                 ipe.signed_policy(POLICY_FIXTURE_V1_ASSET),
             ),
             expect=0,
+            check=partial(checks.policy_version_is, POLICY_FIXTURE_NAME, POLICY_FIXTURE_V1_VERSION),
+        ),
+        Case(
+            id="policy_update_equal_estale",
+            setup=(partial(steps.deploy_policy, POLICY_FIXTURE_V1_ASSET),),
+            trigger=partial(
+                triggers.write_node,
+                "update",
+                POLICY_FIXTURE_NAME,
+                ipe.signed_policy(POLICY_FIXTURE_V1_ASSET),
+            ),
+            expect=errno.ESTALE,
             check=partial(checks.policy_version_is, POLICY_FIXTURE_NAME, POLICY_FIXTURE_V1_VERSION),
         ),
     )
