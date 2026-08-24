1b7ae6129b65ffa1194bacf72fa3c7f17c239227
Fan Wu <wufan@kernel.org>
2026-08-23 19:47:34 -0700

    ipe: test two default rules for one operation
    Load a policy whose text exercises this parser decision point and require EBADMSG.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/policy_text.py
+++ suite/cases/policy_text.py
@@ -228,4 +228,16 @@ def build():
             expect=errno.EBADMSG,
             check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
         ),
+        Case(
+            id="text_rule_duplicate_op_default_ebadmsg",
+            setup=(),
+            trigger=partial(
+                triggers.write_node,
+                "new_policy",
+                None,
+                ipe.signed_policy("policy_text/rule_duplicate_op_default"),
+            ),
+            expect=errno.EBADMSG,
+            check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
+        ),
     )
