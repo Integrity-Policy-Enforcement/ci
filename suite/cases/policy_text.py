c77ff042b54bf1fe60fc39b3da70f9ca7ea10e9b
Fan Wu <wufan@kernel.org>
2026-08-23 19:47:20 -0700

    ipe: test a default rule carrying a property
    Load a policy whose text exercises this parser decision point and require EBADMSG.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/policy_text.py
+++ suite/cases/policy_text.py
@@ -204,4 +204,16 @@ def build():
             expect=errno.EBADMSG,
             check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
         ),
+        Case(
+            id="text_rule_default_with_property_ebadmsg",
+            setup=(),
+            trigger=partial(
+                triggers.write_node,
+                "new_policy",
+                None,
+                ipe.signed_policy("policy_text/rule_default_with_property"),
+            ),
+            expect=errno.EBADMSG,
+            check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
+        ),
     )
