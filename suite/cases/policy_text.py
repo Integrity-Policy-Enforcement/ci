6c243da217c47f9c459054400e23f63e01279189
Fan Wu <wufan@kernel.org>
2026-08-23 19:45:38 -0700

    ipe: test a policy version with two components
    Load a policy whose text exercises this parser decision point and require EBADMSG.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/policy_text.py
+++ suite/cases/policy_text.py
@@ -96,4 +96,16 @@ def build():
             expect=errno.EBADMSG,
             check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
         ),
+        Case(
+            id="text_version_two_parts_ebadmsg",
+            setup=(),
+            trigger=partial(
+                triggers.write_node,
+                "new_policy",
+                None,
+                ipe.signed_policy("policy_text/version_two_parts"),
+            ),
+            expect=errno.EBADMSG,
+            check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
+        ),
     )
