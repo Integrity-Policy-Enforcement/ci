1f2dd0e7e8cf6becffe45957fd3ef1fed86488a1
Fan Wu <wufan@kernel.org>
2026-08-23 19:46:51 -0700

    ipe: test a rule with an unknown operation
    Load a policy whose text exercises this parser decision point and require EBADMSG.
    
    Signed-off-by: Fan Wu <wufan@kernel.org>

--- suite/cases/policy_text.py
+++ suite/cases/policy_text.py
@@ -156,4 +156,16 @@ def build():
             expect=errno.ERANGE,
             check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
         ),
+        Case(
+            id="text_rule_unknown_op_ebadmsg",
+            setup=(),
+            trigger=partial(
+                triggers.write_node,
+                "new_policy",
+                None,
+                ipe.signed_policy("policy_text/rule_unknown_op"),
+            ),
+            expect=errno.EBADMSG,
+            check=partial(checks.policy_present_is, TEXT_POLICY_NAME, False),
+        ),
     )
